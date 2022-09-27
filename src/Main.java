5cc3e75c450e6aee7531aea0daf8be8f22aae07d
Kanyaton Somjit <sita@MacBook-Pro-som-tillhor-Kanyaton.local>
2022-09-27 11:07:25 +0200

    more code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner userInput = new Scanner(System.in);
 
         GameBoard gameBoard = new GameBoard();
